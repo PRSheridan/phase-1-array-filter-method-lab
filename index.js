3a3c31fa4e6652f54c1027eca90c7156c3f0a7ec
Phil <philrsheridan@gmail.com>
2023-10-01 20:49:53 -0400

    passed 7 tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const findMatching = function(driverNames, driver) {
+    let matches = driverNames.filter(function (name) {
+        return name.toLowerCase() == driver.toLowerCase();
+    });
+    return matches;
+}
+
+const fuzzyMatch = function(driverNames, driver) {
+    let matches = driverNames.filter(function (name) {
+        return name[0].toLowerCase() == driver[0].toLowerCase();
+    });
+    return matches;
+}
+
+const matchName = function(driverNames, driver) {
+    console.log(driverNames)
+    let matches = driverNames.filter(function (individual) {
+        return individual.name == driver;
+    });
+    return matches;
+}
